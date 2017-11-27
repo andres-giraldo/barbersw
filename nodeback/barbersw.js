febe72d87e2bb8f4d15215dc35f7e0910f36f122
andresg-s4ds <andresg@s4ds.com>
2017-11-27 11:04:01 -0500

    Version inicial de los proyectos para software de barbería

---
+++ nodeback/barbersw.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const newRoutes = require('./src/routes/new-routes');
+
+mongoose.connect("mongodb://localhost/barbersw", {
+    socketTimeoutMS: 0,
+    keepAlive: true,
+    reconnectTries: 30
+});
+
+mongoose.connection.on("error", () => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running.");
+    process.exit();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+
+let apiRoutes = express.Router();
+apiRoutes.use(function (request, response, next) {
+    next();
+});
+app.use(apiRoutes);
+
+newRoutes(app);
+app.listen(1050);
