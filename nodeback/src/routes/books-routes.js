0eddd2fbd0987b1f1e6d74bfe048d27ae33ec761
andresg-s4ds <andresg@s4ds.com>
2017-11-30 09:28:40 -0500

    Depuración de archivos no utilizados en los proyectos

--- nodeback/src/routes/books-routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    let booksController = require('../controllers/books-controller');
-
-    app.route("/books")
-        .get(booksController.getBook)
-        .post(booksController.saveBook)
-        .delete(booksController.deleteBook);
-};
