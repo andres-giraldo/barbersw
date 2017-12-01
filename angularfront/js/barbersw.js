75df5172b5da3134fe6cbc6f85946135eb23a0e6
Luis Miguel Botero <lmiguel1995@gmail.com>
2017-11-30 22:16:36 -0500

    Commit

--- angularfront/js/barbersw.js
+++ angularfront/js/barbersw.js
@@ -99,7 +99,6 @@ app.controller('cutsController', ['$scope', '$http', function ($scope, $http) {
     }
 
     $scope.saveCut = function () {
-        aler("Entro");
         var img = new Buffer($scope.cut.cutImage, 'base64');
         $http({
             method: 'POST',
